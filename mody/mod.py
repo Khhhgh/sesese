0d283729296132a5ea6bb9041f19cd0c20069b8b
Khhhgh <103472630+Khhhgh@users.noreply.github.com>
2023-09-14 01:07:14 +0300

    Update mod.py

--- mody/mod.py
+++ mody/mod.py
@@ -6,8 +6,8 @@ from mody.get_info import token, sudo_info, get_bot
 
 Bot = Client(
     'MainBot',
-    27786450,
-    '1fb7b1af2837205d7ce8d77cefc0acbd',
+    25996320,
+    '772cefc3a92ed382b6c24adbd0d3ea26',
     bot_token=token,
     plugins=dict(root='plugins/bot')
 )
@@ -15,8 +15,8 @@ Bot = Client(
 
 sudo_client = Client(
     'MainUser',
-    27786450,
-    '1fb7b1af2837205d7ce8d77cefc0acbd',
+    25996320,
+    '772cefc3a92ed382b6c24adbd0d3ea26',
     session_string=db.get(f'{get_bot.id}:{sudo_info.id}:session'),
     plugins=dict(root='plugins/user')
 )
